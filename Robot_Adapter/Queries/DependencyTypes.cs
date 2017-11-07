a9d7e1e60da5cf526cbceebaee2897198d008cff
Rob May <rob.may@burohappold.com>
2017-11-07 06:22:35 +0000

    Updates for Puria

---
+++ Robot_Adapter/Queries/DependencyTypes.cs
@@ -0,0 +1,44 @@
+﻿using BH.oM.Materials;
+using BH.oM.Structural.Elements;
+using BH.oM.Structural.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Adapter.Robot
+{
+    public partial class RobotAdapter
+    {
+        /***************************************************/
+        /**** BHoM Adapter Interface                    ****/
+        /***************************************************/
+
+        protected override List<Type> GetDependencyTypes<T>()
+        {
+            Type type = typeof(T);
+
+            if (m_DependencyTypes.ContainsKey(type))
+                return m_DependencyTypes[type];
+
+            else if (m_DependencyTypes.ContainsKey(type.BaseType))
+                return m_DependencyTypes[type.BaseType];
+
+            else
+                return new List<Type>();
+        }
+
+
+        /***************************************************/
+        /**** Private Fields                            ****/
+        /***************************************************/
+
+        private static Dictionary<Type, List<Type>> m_DependencyTypes = new Dictionary<Type, List<Type>>
+        {
+            {typeof(Bar), new List<Type> { typeof(Node) } },
+            {typeof(SectionProperty), new List<Type> { typeof(Material) } }
+        };
+
+    }
+}

c91019a43d815a37cd5159bf8b5d02d0d9d2ed16
Puria Hesari <Puria.Hesari@burohappold.com>
2018-02-02 11:45:21 +0000

    pushing nodes

--- Robot_Adapter/CRUD/Delete.cs
+++ Robot_Adapter/CRUD/Delete.cs
@@ -5,7 +5,7 @@ using BH.oM.Structural.Design;
 
 namespace BH.Adapter.Robot
 {
-    public partial class RobotAdapter : BHoMAdapter
+    public partial class RobotAdapter
     {
 
         /***************************************************/
@@ -14,10 +14,10 @@ namespace BH.Adapter.Robot
 
         protected override int Delete(Type type, IEnumerable<object> ids)
         {
-            if (type == typeof(DesignGroup))
-            {
-                DeleteSteelDesignGroups(ids as List<int>);
-            }
+            //if (type == typeof(DesignGroup))
+            //{
+            //    DeleteSteelDesignGroups(ids as List<int>);
+            //}
                 return 0;
         }
 
@@ -25,39 +25,39 @@ namespace BH.Adapter.Robot
         /**** Protected Methods                         ****/
         /***************************************************/
 
-        public void DeleteSteelDesignGroups(List<int> designGroupNumbers = null)
-        {
-            RobotApplication robot = this.RobotApplication;
-            RDimServer RDServer = this.RobotApplication.Kernel.GetExtension("RDimServer");
-            RDServer.Mode = RobotOM.IRDimServerMode.I_DSM_STEEL;
-            RDimGroups RDGroups = RDServer.GroupsService;
+        //public void DeleteSteelDesignGroups(List<int> designGroupNumbers = null)
+        //{
+        //    RobotApplication robot = this.RobotApplication;
+        //    RDimServer RDServer = this.RobotApplication.Kernel.GetExtension("RDimServer");
+        //    RDServer.Mode = RobotOM.IRDimServerMode.I_DSM_STEEL;
+        //    RDimGroups RDGroups = RDServer.GroupsService;
 
-            if (designGroupNumbers != null)
-            {
-                foreach (int designGroupNumber in designGroupNumbers)
-                {
-                    _DeleteSteelDesignGroup(designGroupNumber);
-                }
-            }
-            else
-            {
-                for (int i = 0; i <= RDGroups.Count - 1; i++)
-                {
-                    int designGroupNumber = RDGroups.GetUserNo(i);
-                    _DeleteSteelDesignGroup(designGroupNumber);
+        //    if (designGroupNumbers != null)
+        //    {
+        //        foreach (int designGroupNumber in designGroupNumbers)
+        //        {
+        //            _DeleteSteelDesignGroup(designGroupNumber);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        for (int i = 0; i <= RDGroups.Count - 1; i++)
+        //        {
+        //            int designGroupNumber = RDGroups.GetUserNo(i);
+        //            _DeleteSteelDesignGroup(designGroupNumber);
 
-                }
-            }
-        }
+        //        }
+        //    }
+        //}
 
-        private void _DeleteSteelDesignGroup(int designGroupNumber)
-        {
-            RobotApplication robot = this.RobotApplication;
-            RDimServer RDServer = this.RobotApplication.Kernel.GetExtension("RDimServer");
-            RDServer.Mode = RobotOM.IRDimServerMode.I_DSM_STEEL;
-            RDimGroups RDGroups = RDServer.GroupsService;
-            RDGroups.Delete(designGroupNumber);            
-        }
+        //private void _DeleteSteelDesignGroup(int designGroupNumber)
+        //{
+        //    RobotApplication robot = this.RobotApplication;
+        //    RDimServer RDServer = this.RobotApplication.Kernel.GetExtension("RDimServer");
+        //    RDServer.Mode = RobotOM.IRDimServerMode.I_DSM_STEEL;
+        //    RDimGroups RDGroups = RDServer.GroupsService;
+        //    RDGroups.Delete(designGroupNumber);            
+        //}
 
 
 

5ff7925e263def5ddc1729f84088456cb69f612e
Rob May <rob.may@burohappold.com>
2018-04-25 07:50:34 +0100

    Loadcase number create and delete methods updated

--- Robot_Adapter/CRUD/Create.cs
+++ Robot_Adapter/CRUD/Create.cs
@@ -224,7 +224,7 @@ namespace BH.Adapter.Robot
             {
                 int subNature;
                 IRobotCaseNature rNature = BH.Engine.Robot.Convert.RobotLoadNature(caseList[i], out subNature);
-                m_RobotApplication.Project.Structure.Cases.CreateSimple(System.Convert.ToInt32(caseList[i].CustomData[AdapterId]), caseList[i].Name, rNature, IRobotCaseAnalizeType.I_CAT_STATIC_LINEAR);
+                m_RobotApplication.Project.Structure.Cases.CreateSimple(caseList[i].Number, caseList[i].Name, rNature, IRobotCaseAnalizeType.I_CAT_STATIC_LINEAR);
                 IRobotSimpleCase sCase = caseServer.Get(System.Convert.ToInt32(caseList[i].CustomData[AdapterId])) as IRobotSimpleCase;
                 if (subNature >= 0)
                     sCase.SetNatureExt(subNature);

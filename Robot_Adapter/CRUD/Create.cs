cb9333d3635b966418da9ef38b7b9486c9a26de6
Rob May <rob.may@burohappold.com>
2018-04-25 08:23:33 +0100

    Update to loadcase create method
    @puriasafarihesari  there are some 'multiOp' functions that we should use for generation of elements not through the funky cache (through COM). For bars, nodes, cases and i think lots of other labels, there is typically a 'multiOp'. You start it before your create loop and finish at the end, and Robot API automatically caches. It's usually not as fast as our other cache method, but it does speed up COM calls, particularly for cases (we specifically requested this as we were creating up to 20k cases previously).

--- Robot_Adapter/CRUD/Create.cs
+++ Robot_Adapter/CRUD/Create.cs
@@ -220,15 +220,17 @@ namespace BH.Adapter.Robot
             List<Loadcase> caseList = loadCase.ToList();
             IRobotCaseServer caseServer = m_RobotApplication.Project.Structure.Cases;
 
+            m_RobotApplication.Project.Structure.Cases.BeginMultiOperation();
             for (int i = 0; i < caseList.Count; i++)
-            {
+            {                
                 int subNature;
                 IRobotCaseNature rNature = BH.Engine.Robot.Convert.RobotLoadNature(caseList[i], out subNature);
-                m_RobotApplication.Project.Structure.Cases.CreateSimple(caseList[i].Number, caseList[i].Name, rNature, IRobotCaseAnalizeType.I_CAT_STATIC_LINEAR);
-                IRobotSimpleCase sCase = caseServer.Get(System.Convert.ToInt32(caseList[i].CustomData[AdapterId])) as IRobotSimpleCase;
+                m_RobotApplication.Project.Structure.Cases.CreateSimple(caseList[i].Number, caseList[i].Name, rNature, IRobotCaseAnalizeType.I_CAT_STATIC_LINEAR);                
+                IRobotSimpleCase sCase = caseServer.Get(caseList[i].Number) as IRobotSimpleCase;
                 if (subNature >= 0)
                     sCase.SetNatureExt(subNature);
             }
+            m_RobotApplication.Project.Structure.Cases.EndMultiOperation();
             return true;
         }
 

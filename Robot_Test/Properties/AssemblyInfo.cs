d2b797109e1e4156d3f8b1c1d7f6f0e8d3088f93
adecler <arnaud.declercq@burohappold.com>
2018-12-21 16:28:58 +0800

    Clear the info on assembly company

--- Robot_Test/Properties/AssemblyInfo.cs
+++ Robot_Test/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("RobotTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("BuroHappold")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RobotTest")]
 [assembly: AssemblyCopyright("Copyright © https://github.com/BHoM")]
 [assembly: AssemblyTrademark("")]

438e94e6eed074f0fddb7ae02412bc5e3418bc2d
BHoMBot <BHoMBot@burohappold.com>
2021-01-08 12:51:14 +0000

    Remove PreviousVersion attributes for previous milestones

--- Robot_Engine/Query/ToDictionaryDistinctCheck.cs
+++ Robot_Engine/Query/ToDictionaryDistinctCheck.cs
@@ -35,7 +35,6 @@ namespace BH.Engine.Adapters.Robot
         /****             Public Methods                ****/
         /***************************************************/
 
-        [PreviousVersion("3.2", "BH.Engine.Robot.Query.ToDictionaryDistinctCheck(System.Collections.Generic.IEnumerable<BH.Engine.Robot.T>, System.Func<BH.Engine.Robot.T, System.String>)")]
         public static Dictionary<string, T> ToDictionaryDistinctCheck<T>(this IEnumerable<T> list, Func<T, string> selector)
         {
             var group = list.Where(x => x != null && selector(x) != null).GroupBy(selector);

713bc43f7327a9a1dd702701005f44c2e2e614bb
Isak Naslund <Isak.Naslund@burohappold.com>
2021-09-01 09:41:15 +0200

    Update error message to "type-strong" references

--- Robot_Adapter/AdapterActions/Execute.cs
+++ Robot_Adapter/AdapterActions/Execute.cs
@@ -105,10 +105,9 @@ namespace BH.Adapter.Robot
         {
             if (command.SaveBeforeClose)
             {
-                
                 if (string.IsNullOrEmpty(m_RobotApplication.Project.FileName))
                 {
-                    Engine.Reflection.Compute.RecordError("Application not exited. File does not have a name. Please manually save the file or use the SaveAs command before trying to Exit the application. If you want to close the application anyway, please toggle SaveBeforeClose to false.");
+                    Engine.Reflection.Compute.RecordError($"Application not exited. File does not have a name. Please manually save the file or use the {nameof(SaveAs)} command before trying to Exit the application. If you want to close the application anyway, please toggle {nameof(Exit.SaveBeforeClose)} to false.");
                     return false;
                 }
                 m_RobotApplication.Project.Save();

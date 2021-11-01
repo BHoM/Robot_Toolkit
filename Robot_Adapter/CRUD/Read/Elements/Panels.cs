9822ed6b2de0e7ec5373ee2227bd3417c2f56ecd
Isak Naslund <Isak.Naslund@burohappold.com>
2021-11-01 10:11:33 +0100

    Refactor out panel geometry extraction method to its own method

--- Robot_Adapter/CRUD/Read/Elements/Panels.cs
+++ Robot_Adapter/CRUD/Read/Elements/Panels.cs
@@ -84,122 +84,34 @@ namespace BH.Adapter.Robot
 
                 if (robotPanel.Main.Attribs.Meshed == 1)
                 {
-                    ICurve outline = Convert.IFromRobot(robotPanel.Main.GetGeometry());
-                    List<Opening> openings = new List<Opening>();
+                    panel = ExtractPanelGeometry(robotPanel, robotPanelServer);
 
-                    IRobotCollection robotOpenings = robotPanelServer.GetMany(robotPanel.GetHostedObjects());
-                    for (int j = 1; j <= robotOpenings.Count; j++)
+                    if (panel != null)
                     {
-                        Opening opening;
-                        RobotObjObject robotOpening = (RobotObjObject)robotOpenings.Get(j);
-
-                        // Skipping hosted Beams and Columns - should we keep Slabs and Walls? Normal type for openings seems to be Undefined.
-                        if (robotOpening.StructuralType == IRobotObjectStructuralType.I_OST_BEAM || robotOpening.StructuralType == IRobotObjectStructuralType.I_OST_COLUMN)
-                            continue;
-
-                        ICurve openingOutline = Convert.IFromRobot(robotOpening.Main.GetGeometry());
-
-                        if (openingOutline == null)
+                        if (robotPanel.HasLabel(IRobotLabelType.I_LT_PANEL_THICKNESS) != 0)
                         {
-                            Engine.Reflection.Compute.RecordError($"Failed to extract the outline geometry for Opening with id {robotOpening.Number} on Panel with id {robotPanel.Number}.");
-                            opening = new Opening();
-                        }
-                        else if (openingOutline is PolyCurve && (openingOutline as PolyCurve).Curves.Count == 0)
-                        {
-                            Engine.Reflection.Compute.RecordWarning($"Opening with id {robotOpening.Number} on Panel with id {robotPanel.Number} does not contain any geometry.");
-                            opening = new Opening();
-                        }
-                        else
-                        {
-                            opening = BH.Engine.Structure.Create.Opening(openingOutline);
-                        }
-
-                        SetAdapterId(opening, robotOpening.Number);
-                        openings.Add(opening);
-                    }
-                    try
-                    {
-                        if (outline != null)
-                            panel = BH.Engine.Structure.Create.Panel(outline, openings);
-                        else
-                        {
-                            BH.Engine.Reflection.Compute.RecordError($"Failed to extract the outline geometry for Panel with id {robotPanel.Number}.");
-                            panel = new Panel { Openings = openings };
-                        }
-                    }
-                    catch
-                    {
-                        BH.Engine.Reflection.Compute.RecordWarning($"Geometry for Panel with id {robotPanel.Number} not supported.");
-                        continue;
-                    }
-
-                    SetAdapterId(panel, robotPanel.Number);
-
-                    PanelFiniteElementIds feIds = new PanelFiniteElementIds
-                    {
-                        FiniteElementIds = robotPanel.FiniteElems,
-                        NodeIds = robotPanel.Nodes
-                    };
-
-                    panel.Fragments.Add(feIds);
-
-                    try
-                    {
-                        //Get the coordinate system for the panel
-                        double x, y, z; robotPanel.Main.Attribs.GetDirX(out x, out y, out z);
-                        Vector coordXAxis = BH.Engine.Geometry.Create.Vector(x, y, z);
-                        Vector coordZAxis = panel.Normal();
-
-                        if (coordZAxis != null)
-                        {
-                            bool flip = robotPanel.Main.Attribs.DirZ == 1;
-                            flip = Convert.FromRobotCheckFlipNormal(coordZAxis, flip);
-
-                            if (flip)
+                            string propName = robotPanel.GetLabelName(IRobotLabelType.I_LT_PANEL_THICKNESS);
+                            if (!surfaceProperties.ContainsKey(propName))
                             {
-                                coordZAxis = coordZAxis.Reverse();
-                                FlipOutline(panel);
-                            }
-
-                            //Set local orientation
-                            panel.OrientationAngle = Engine.Structure.Compute.OrientationAngleAreaElement(coordZAxis, coordXAxis);
-                        }
-                        else
-                        {
-                            Engine.Reflection.Compute.RecordWarning("Failed to extract local Orientations for at least one Panel.");
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        string message = "Failed to get local orientations for a Panel. Exception message: " + e.Message;
-
-                        if (!string.IsNullOrEmpty(e.InnerException?.Message))
-                        {
-                            message += "\nInnerException: " + e.InnerException.Message;
-                        }
-
-                        Engine.Reflection.Compute.RecordWarning(message);
-                    }
-
-                    if (robotPanel.HasLabel(IRobotLabelType.I_LT_PANEL_THICKNESS) != 0)
-                    {
-                        string propName = robotPanel.GetLabelName(IRobotLabelType.I_LT_PANEL_THICKNESS);
-                        if (!surfaceProperties.ContainsKey(propName))
-                        {
-                            ISurfaceProperty surfaceProperty = ReadSurfacePropertyFromPanel(robotPanel, materials);
-                            if (surfaceProperty != null && surfaceProperty.Material == null)
-                            {
-                                surfaceProperty.Material = ReadMaterialFromPanel(robotPanel);
-                                if (surfaceProperty.Material != null && !string.IsNullOrEmpty(surfaceProperty.Material.Name) && !materials.ContainsKey(surfaceProperty.Name))
+                                ISurfaceProperty surfaceProperty = ReadSurfacePropertyFromPanel(robotPanel, materials);
+                                if (surfaceProperty != null && surfaceProperty.Material == null)
                                 {
-                                    materials.Add(surfaceProperty.Material.Name, surfaceProperty.Material);
+                                    surfaceProperty.Material = ReadMaterialFromPanel(robotPanel);
+                                    if (surfaceProperty.Material != null && !string.IsNullOrEmpty(surfaceProperty.Material.Name) && !materials.ContainsKey(surfaceProperty.Name))
+                                    {
+                                        materials.Add(surfaceProperty.Material.Name, surfaceProperty.Material);
+                                    }
                                 }
+                                surfaceProperties.Add(propName, surfaceProperty);
                             }
-                            surfaceProperties.Add(propName, surfaceProperty);
+                            panel.Property = surfaceProperties[propName];
                         }
-                        panel.Property = surfaceProperties[propName];
+                        panels.Add(panel);
                     }
-                    panels.Add(panel);
+                    else
+                    {
+                        Engine.Reflection.Compute.RecordError($"Failed to extract Panel with id {robotPanel.Number}.");
+                    }
                 }
             }
 
@@ -216,45 +128,7 @@ namespace BH.Adapter.Robot
 
                 if (robotCladdingPanel.HasLabel(IRobotLabelType.I_LT_CLADDING) != 0)
                 {
-                    List<Opening> claddingOpenings = new List<Opening>();
-                    IRobotCollection robotCladdingOpenings = robotPanelServer.GetMany(robotCladdingPanel.GetHostedObjects());
-                    for (int j = 1; j <= robotCladdingOpenings.Count; j++)
-                    {
-                        Opening opening;
-                        RobotObjObject robotOpening = (RobotObjObject)robotCladdingOpenings.Get(j);
-                        ICurve openingOutline = Convert.IFromRobot(robotOpening.Main.GetGeometry());
-
-                        if (openingOutline != null)
-                            opening = BH.Engine.Structure.Create.Opening(openingOutline);
-                        else
-                        {
-                            Engine.Reflection.Compute.RecordError($"Failed to extract the outline geometry for Opening with id {robotOpening.Number} on Panel with id {robotCladdingPanel.Number}.");
-                            opening = new Opening();
-                        }
-
-                        SetAdapterId(opening, robotOpening.Number);
-                        claddingOpenings.Add(opening);
-                    }
-                    if (robotCladdingPanel.Main.Attribs.Meshed == 1)
-                    {
-                        ICurve outline = Convert.IFromRobot(robotCladdingPanel.Main.GetGeometry());
-                        try
-                        {
-                            if (outline != null)
-                                claddingPanel = BH.Engine.Structure.Create.Panel(outline, claddingOpenings);
-                            else
-                            {
-                                BH.Engine.Reflection.Compute.RecordError($"Failed to extract the outline geometry for Panel with id {robotCladdingPanel.Number}.");
-                                claddingPanel = new Panel { Openings = claddingOpenings };
-                            }
-                        }
-                        catch
-                        {
-                            BH.Engine.Reflection.Compute.RecordWarning($"Geometry for Panel with id {robotCladdingPanel.Number} not supported.");
-                            continue;
-                        }
-
-                    }
+                    claddingPanel = ExtractPanelGeometry(robotCladdingPanel, robotPanelServer);
                     if (claddingPanel != null)
                     {
                         SetAdapterId(claddingPanel, robotCladdingPanel.Number);
@@ -277,6 +151,118 @@ namespace BH.Adapter.Robot
                 }
             }
             return panels;
+        }
+
+        /***************************************************/
+
+        private Panel ExtractPanelGeometry(RobotObjObject robotPanel, IRobotObjObjectServer robotPanelServer)
+        {
+            Panel panel = null;
+            ICurve outline = Convert.IFromRobot(robotPanel.Main.GetGeometry());
+            List<Opening> openings = new List<Opening>();
+
+            IRobotCollection robotOpenings = robotPanelServer.GetMany(robotPanel.GetHostedObjects());
+            for (int j = 1; j <= robotOpenings.Count; j++)
+            {
+                Opening opening;
+                RobotObjObject robotOpening = (RobotObjObject)robotOpenings.Get(j);
+
+                // Skipping hosted Beams and Columns - should we keep Slabs and Walls? Normal type for openings seems to be Undefined.
+                if (robotOpening.StructuralType == IRobotObjectStructuralType.I_OST_BEAM || robotOpening.StructuralType == IRobotObjectStructuralType.I_OST_COLUMN)
+                    continue;
+
+                ICurve openingOutline = Convert.IFromRobot(robotOpening.Main.GetGeometry());
+
+                if (openingOutline == null)
+                {
+                    Engine.Reflection.Compute.RecordError($"Failed to extract the outline geometry for Opening with id {robotOpening.Number} on Panel with id {robotPanel.Number}.");
+                    opening = new Opening();
+                }
+                else if (openingOutline is PolyCurve && (openingOutline as PolyCurve).Curves.Count == 0)
+                {
+                    Engine.Reflection.Compute.RecordWarning($"Opening with id {robotOpening.Number} on Panel with id {robotPanel.Number} does not contain any geometry.");
+                    opening = new Opening();
+                }
+                else
+                {
+                    opening = BH.Engine.Structure.Create.Opening(openingOutline);
+                }
+
+                SetAdapterId(opening, robotOpening.Number);
+                openings.Add(opening);
+            }
+            bool geometrySet = true;
+            try
+            {
+                if (outline != null)
+                    panel = BH.Engine.Structure.Create.Panel(outline, openings);
+
+                if (panel == null)
+                {
+                    BH.Engine.Reflection.Compute.RecordError($"Failed to extract the outline geometry for Panel with id {robotPanel.Number}.");
+                    panel = new Panel { Openings = openings };
+                    geometrySet = false;
+                }
+            }
+            catch
+            {
+                BH.Engine.Reflection.Compute.RecordWarning($"Geometry for Panel with id {robotPanel.Number} not supported.");
+                panel = new Panel { Openings = openings };
+                geometrySet = false;
+            }
+
+            SetAdapterId(panel, robotPanel.Number);
+
+            PanelFiniteElementIds feIds = new PanelFiniteElementIds
+            {
+                FiniteElementIds = robotPanel.FiniteElems,
+                NodeIds = robotPanel.Nodes
+            };
+
+            panel.Fragments.Add(feIds);
+
+            if (geometrySet)
+            {
+                try
+                {
+                    //Get the coordinate system for the panel
+                    double x, y, z; robotPanel.Main.Attribs.GetDirX(out x, out y, out z);
+                    Vector coordXAxis = BH.Engine.Geometry.Create.Vector(x, y, z);
+                    Vector coordZAxis = panel.Normal();
+
+                    if (coordZAxis != null)
+                    {
+                        bool flip = robotPanel.Main.Attribs.DirZ == 1;
+                        flip = Convert.FromRobotCheckFlipNormal(coordZAxis, flip);
+
+                        if (flip)
+                        {
+                            coordZAxis = coordZAxis.Reverse();
+                            FlipOutline(panel);
+                        }
+
+                        //Set local orientation
+                        panel.OrientationAngle = Engine.Structure.Compute.OrientationAngleAreaElement(coordZAxis, coordXAxis);
+                    }
+                    else
+                    {
+                        Engine.Reflection.Compute.RecordWarning("Failed to extract local Orientations for at least one Panel.");
+                    }
+                }
+                catch (Exception e)
+                {
+                    string message = "Failed to get local orientations for a Panel. Exception message: " + e.Message;
+
+                    if (!string.IsNullOrEmpty(e.InnerException?.Message))
+                    {
+                        message += "\nInnerException: " + e.InnerException.Message;
+                    }
+
+                    Engine.Reflection.Compute.RecordWarning(message);
+                }
+            }
+
+            return panel;
         }
 
         /***************************************************/

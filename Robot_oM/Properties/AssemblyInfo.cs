7f328e79b53df36f575c009f3416fa5714e0b5f0
FraserGreenroyd <fraser.greenroyd@burohappold.com>
2022-07-18 17:38:03 +0100

    Update Assembly Information

--- Robot_oM/Properties/AssemblyInfo.cs
+++ Robot_oM/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Robot_oM")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/BHoM/Robot_Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Robot_oM")]

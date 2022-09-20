02451c7ebfcae63f3e0a0be8b506130c8f2fb00c
Isak Naslund <Isak.Naslund@burohappold.com>
2022-09-20 08:39:53 +0200

    Splitting up SurfaceProperty convert method

--- Robot_Adapter/Convert/FromRobot/Properties/SurfaceProperty.cs
+++ Robot_Adapter/Convert/FromRobot/Properties/SurfaceProperty.cs
@@ -44,9 +44,6 @@ namespace BH.Adapter.Robot
                 return null;
             }
 
-            ISurfaceProperty bhomProperty = null;
-            IMaterialFragment mat = null;
-
             if (robotLabelName == "")
                 robotLabelName = robotLabel.Name;
 
@@ -62,75 +59,13 @@ namespace BH.Adapter.Robot
                         return null;
                     }
 
-                    mat = null;
+                    IMaterialFragment mat = null;
                     if (material.ContainsKey(data.MaterialName))
                         mat = material[data.MaterialName];
                     else
                         BH.Engine.Base.Compute.RecordEvent("SurfaceProperty " + robotLabelName + " has no material assigned", oM.Base.Debugging.EventType.Warning);
 
-                    bhomProperty = null;
-
-                    switch (data.ThicknessType)
-                    {
-                        case IRobotThicknessType.I_TT_HOMOGENEOUS:
-                            IRobotThicknessHomoData homoData = data.Data;
-                            switch (homoData.Type)
-                            {
-                                case IRobotThicknessHomoType.I_THT_CONSTANT:
-                                    bhomProperty = new ConstantThickness { Name = robotLabelName, Thickness = homoData.ThickConst, Material = mat };
-                                    break;
-                                case IRobotThicknessHomoType.I_THT_VARIABLE_ON_PLANE:
-                                    break;
-                            }
-                            break;
-                        case IRobotThicknessType.I_TT_ORTHOTROPIC:
-
-                            IRobotThicknessOrthoData orthoData = data.Data;
-                            ConstantThickness BHoMConstThick = new ConstantThickness();
-
-                            switch (orthoData.Type)
-                            {
-                                case IRobotThicknessOrthoType.I_TOT_ONE_SIDED_UNIDIR_RIBS:
-                                    bhomProperty = new Ribbed { Name = robotLabelName, Thickness = orthoData.H, TotalDepth = orthoData.HA, StemWidth = orthoData.A1, Spacing = orthoData.A, Direction = orthoData.DirType == IRobotThicknessOrthoDirType.I_TODT_DIR_X ? PanelDirection.X : PanelDirection.Y, Material = mat };
-                                    break;
-                                case IRobotThicknessOrthoType.I_TOT_ONE_SIDED_BIDIR_RIBS:
-                                    bhomProperty = new Waffle { Name = robotLabelName, Thickness = orthoData.H, TotalDepthX = orthoData.HA, TotalDepthY = orthoData.HB, StemWidthX = orthoData.A1, StemWidthY = orthoData.B1, SpacingX = orthoData.A, SpacingY = orthoData.B, Material = mat };
-                                    break;
-                                case IRobotThicknessOrthoType.I_TOT_MATERIAL:
-                                    bhomProperty = new ConstantThickness { Name = robotLabelName, Thickness = orthoData.H, Material = mat };
-                                    double n1 = orthoData.N1;
-                                    double n2 = orthoData.N2;
-                                    bhomProperty = bhomProperty.ApplyModifiers(n1, Math.Sqrt(n1 + n2), n2, n1, Math.Sqrt(n1 + n2), n2, n1, n2);
-                                    break;
-                                case IRobotThicknessOrthoType.I_TOT_UNIDIR_BOX_FLOOR:
-                                case IRobotThicknessOrthoType.I_TOT_SLAB_ON_TRAPEZOID_PLATE:
-                                case IRobotThicknessOrthoType.I_TOT_GRILLAGE:
-                                case IRobotThicknessOrthoType.I_TOT_BIDIR_BOX_FLOOR:
-                                case IRobotThicknessOrthoType.I_TOT_HOLLOW_CORE_SLAB:
-                                case IRobotThicknessOrthoType.I_TOT_USER:
-                                    bhomProperty = new ConstantThickness { Name = robotLabelName, Thickness = double.NaN, Material = mat };
-                                    BH.Engine.Base.Compute.RecordWarning("Thickness property '" + robotLabelName + "' of type " + orthoData.Type + " Not yet supported. A constant thickness property with NaN thickness has been created in its place");
-                                    break;
-                                default:
-                                    bhomProperty = new ConstantThickness { Name = robotLabelName, Thickness = orthoData.H, Material = mat };
-                                    SurfacePropertyModifier modifier = new SurfacePropertyModifier
-                                    {
-                                        FXX = orthoData.HA,
-                                        FXY = orthoData.HB,
-                                        FYY = orthoData.H0,
-                                        MXX = orthoData.HC,
-                                        MXY = orthoData.A,
-                                        MYY = orthoData.A1,
-                                        VXZ = orthoData.A2,
-                                        VYZ = orthoData.B,
-                                        Mass = 1,   //Cant extract mass via the API
-                                        Weight = orthoData.B1
-                                    };
-                                    bhomProperty.Fragments.Add(modifier);
-                                    break;
-                            }
-                            break;
-                    }
+                    return robotLabel.FromRobot(data, mat, robotLabelName);
                 }
                 else if (robotLabel.Type == IRobotLabelType.I_LT_CLADDING)
                 {
@@ -141,40 +76,156 @@ namespace BH.Adapter.Robot
                         Engine.Base.Compute.RecordWarning("Failed to extract at least one ISurfaceProperty from Robot.");
                         return null;
                     }
-
-                    switch (claddData.Type)
-                    {
-                        case IRobotCladdingType.I_CT_X:
-                            bhomProperty = new LoadingPanelProperty
-                            {
-                                LoadApplication = LoadPanelSupportConditions.TwoSides,
-                                Name = robotLabelName,
-                                ReferenceEdge = 1
-                            };
-                            break;
-
-                        case IRobotCladdingType.I_CT_Y:
-                            bhomProperty = new LoadingPanelProperty
-                            {
-                                LoadApplication = LoadPanelSupportConditions.TwoSides,
-                                Name = robotLabelName,
-                                ReferenceEdge = 2
-                            };
-                            break;
-
-                        case IRobotCladdingType.I_CT_XY:
-                            bhomProperty = new LoadingPanelProperty
-                            {
-                                LoadApplication = LoadPanelSupportConditions.AllSides,
-                                Name = robotLabelName,
-                                ReferenceEdge = 1
-                            };
-                            break;
-
-                        default:
-                            break;
-                    }
+                    return robotLabel.FromRobot(claddData, robotLabelName);
                 }
+                return null;
+            }
+            catch (Exception)
+            {
+                Engine.Base.Compute.RecordWarning("Failed to extract at least one ISurfaceProperty from Robot.");
+                return null;
+            }
+        }
+
+        /***************************************************/
+
+        public static ISurfaceProperty FromRobot(this IRobotLabel robotLabel, IRobotThicknessData data, IMaterialFragment material, string robotLabelName = "")
+        {
+
+            if (robotLabelName == "")
+                robotLabelName = robotLabel.Name;
+            try
+            {
+                if (data == null)
+                {
+                    Engine.Base.Compute.RecordWarning("Failed to extract at least one ISurfaceProperty from Robot.");
+                    return null;
+                }
+
+               ISurfaceProperty  bhomProperty = null;
+
+                switch (data.ThicknessType)
+                {
+                    case IRobotThicknessType.I_TT_HOMOGENEOUS:
+                        IRobotThicknessHomoData homoData = data.Data;
+                        switch (homoData.Type)
+                        {
+                            case IRobotThicknessHomoType.I_THT_CONSTANT:
+                                bhomProperty = new ConstantThickness { Name = robotLabelName, Thickness = homoData.ThickConst, Material = material };
+                                break;
+                            case IRobotThicknessHomoType.I_THT_VARIABLE_ON_PLANE:
+                                break;
+                        }
+                        break;
+                    case IRobotThicknessType.I_TT_ORTHOTROPIC:
+
+                        IRobotThicknessOrthoData orthoData = data.Data;
+                        ConstantThickness BHoMConstThick = new ConstantThickness();
+
+                        switch (orthoData.Type)
+                        {
+                            case IRobotThicknessOrthoType.I_TOT_ONE_SIDED_UNIDIR_RIBS:
+                                bhomProperty = new Ribbed { Name = robotLabelName, Thickness = orthoData.H, TotalDepth = orthoData.HA, StemWidth = orthoData.A1, Spacing = orthoData.A, Direction = orthoData.DirType == IRobotThicknessOrthoDirType.I_TODT_DIR_X ? PanelDirection.X : PanelDirection.Y, Material = material };
+                                break;
+                            case IRobotThicknessOrthoType.I_TOT_ONE_SIDED_BIDIR_RIBS:
+                                bhomProperty = new Waffle { Name = robotLabelName, Thickness = orthoData.H, TotalDepthX = orthoData.HA, TotalDepthY = orthoData.HB, StemWidthX = orthoData.A1, StemWidthY = orthoData.B1, SpacingX = orthoData.A, SpacingY = orthoData.B, Material = material };
+                                break;
+                            case IRobotThicknessOrthoType.I_TOT_MATERIAL:
+                                bhomProperty = new ConstantThickness { Name = robotLabelName, Thickness = orthoData.H, Material = material };
+                                double n1 = orthoData.N1;
+                                double n2 = orthoData.N2;
+                                bhomProperty = bhomProperty.ApplyModifiers(n1, Math.Sqrt(n1 + n2), n2, n1, Math.Sqrt(n1 + n2), n2, n1, n2);
+                                break;
+                            case IRobotThicknessOrthoType.I_TOT_UNIDIR_BOX_FLOOR:
+                            case IRobotThicknessOrthoType.I_TOT_SLAB_ON_TRAPEZOID_PLATE:
+                            case IRobotThicknessOrthoType.I_TOT_GRILLAGE:
+                            case IRobotThicknessOrthoType.I_TOT_BIDIR_BOX_FLOOR:
+                            case IRobotThicknessOrthoType.I_TOT_HOLLOW_CORE_SLAB:
+                            case IRobotThicknessOrthoType.I_TOT_USER:
+                                bhomProperty = new ConstantThickness { Name = robotLabelName, Thickness = double.NaN, Material = material };
+                                BH.Engine.Base.Compute.RecordWarning("Thickness property '" + robotLabelName + "' of type " + orthoData.Type + " Not yet supported. A constant thickness property with NaN thickness has been created in its place");
+                                break;
+                            default:
+                                bhomProperty = new ConstantThickness { Name = robotLabelName, Thickness = orthoData.H, Material = material };
+                                SurfacePropertyModifier modifier = new SurfacePropertyModifier
+                                {
+                                    FXX = orthoData.HA,
+                                    FXY = orthoData.HB,
+                                    FYY = orthoData.H0,
+                                    MXX = orthoData.HC,
+                                    MXY = orthoData.A,
+                                    MYY = orthoData.A1,
+                                    VXZ = orthoData.A2,
+                                    VYZ = orthoData.B,
+                                    Mass = 1,   //Cant extract mass via the API
+                                    Weight = orthoData.B1
+                                };
+                                bhomProperty.Fragments.Add(modifier);
+                                break;
+                        }
+                        break;
+                }
+
+
+                return bhomProperty;
+            }
+            catch (Exception)
+            {
+                Engine.Base.Compute.RecordWarning("Failed to extract at least one ISurfaceProperty from Robot.");
+                return null;
+            }
+        }
+
+        /***************************************************/
+
+        public static ISurfaceProperty FromRobot(this IRobotLabel robotLabel, IRobotCladdingData claddData, string robotLabelName = "")
+        {
+
+            if (robotLabelName == "")
+                robotLabelName = robotLabel.Name;
+            try
+            {
+                ISurfaceProperty bhomProperty = null;
+
+                if (claddData == null)
+                {
+                    Engine.Base.Compute.RecordWarning("Failed to extract at least one ISurfaceProperty from Robot.");
+                    return null;
+                }
+
+                switch (claddData.Type)
+                {
+                    case IRobotCladdingType.I_CT_X:
+                        bhomProperty = new LoadingPanelProperty
+                        {
+                            LoadApplication = LoadPanelSupportConditions.TwoSides,
+                            Name = robotLabelName,
+                            ReferenceEdge = 1
+                        };
+                        break;
+
+                    case IRobotCladdingType.I_CT_Y:
+                        bhomProperty = new LoadingPanelProperty
+                        {
+                            LoadApplication = LoadPanelSupportConditions.TwoSides,
+                            Name = robotLabelName,
+                            ReferenceEdge = 2
+                        };
+                        break;
+
+                    case IRobotCladdingType.I_CT_XY:
+                        bhomProperty = new LoadingPanelProperty
+                        {
+                            LoadApplication = LoadPanelSupportConditions.AllSides,
+                            Name = robotLabelName,
+                            ReferenceEdge = 1
+                        };
+                        break;
+
+                    default:
+                        break;
+                }
+
                 return bhomProperty;
             }
             catch (Exception)

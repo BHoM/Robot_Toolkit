6ff2bceb9f49ef950e012d259d4149a7c6b9faf4
Isak Naslund <Isak.Naslund@burohappold.com>
2023-01-05 13:13:45 +0100

    fix correct variable name

--- Robot_Adapter/CRUD/Read/Loads/LoadCombinations.cs
+++ Robot_Adapter/CRUD/Read/Loads/LoadCombinations.cs
@@ -49,7 +49,7 @@ namespace BH.Adapter.Robot
                     IRobotCase rCase = rLoadCollection.Get(i) as IRobotCase;
                     if (rCase.Type == IRobotCaseType.I_CT_SIMPLE)   //Pull simple cases to temporary dictionary to be used for assigning to combinations
                     {
-                        Loadcase lCase = new Loadcase { Name = rCaseIn.Name, Number = rCaseIn.Number, Nature = Convert.FromRobot(rCaseIn.Nature) };
+                        Loadcase lCase = new Loadcase { Name = rCase.Name, Number = rCase.Number, Nature = Convert.FromRobot(rCase.Nature) };
                         SetAdapterId(lCase, rCase.Number);
                         pulledCases[lCase.Number] = lCase;
                     }
